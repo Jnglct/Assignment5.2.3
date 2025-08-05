6b47129f05e9065326bbcca31017f0c3b6522883
Jnglct <rogerscole99@gmail.com>
2025-08-05 15:39:54 -0500

    Add project files.

---
+++ Assignment5.2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment5._2._3
+{
+    internal class Program
+    {
+        static void PrintNumbers(int nextNumber)
+        {
+            if (nextNumber == 0)
+            {
+                return;
+            }
+            Console.Write($"{nextNumber} ");
+            nextNumber -= 1;
+            PrintNumbers(nextNumber);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("How many Number to print: ");
+        input:
+            if (!int.TryParse(Console.ReadLine(), out int numberToPrint))
+            {
+                Console.WriteLine("Please enter a valid integer.");
+                goto input;
+            }
+            PrintNumbers(numberToPrint);
+        }
+    }
+}
